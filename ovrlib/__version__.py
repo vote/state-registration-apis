9febc77bacb4d081012d93440fcfc7b7d9703201
Sage Weil <sage@redhat.com>
2020-06-29 14:13:19 -0500

    add setup.py and __version__.py

---
+++ ovrlib/__version__.py
@@ -0,0 +1,12 @@
+"""Version details for ovrlib"""
+
+__title__ = 'ovrlib'
+__description__ = 'Library to interface with per-state online voter regigration APIs'
+__url__ = 'https://github.com/vote/ovrlib'
+__version__ = '0.0.1'
+__author__ = 'Sage Weil'
+__author_email__ = 'sage@newdream.net'
+__license__ = 'Apache 2.0'
+__maintainer__ = 'Sage Weil'
+__maintainer_email__ = 'sage@newdream.net'
+__keywords__ = 'python ovr vote'
