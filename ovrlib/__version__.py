5b11f251f59f4e3a5dcb53d96e7be75654f30ab4
Sage Weil <sage@redhat.com>
2020-06-30 09:29:23 -0500

    pypi metadata fixes

--- ovrlib/__version__.py
+++ ovrlib/__version__.py
@@ -1,8 +1,8 @@
 """Version details for ovrlib"""
 
 __title__ = "ovrlib"
-__description__ = "Library to interface with per-state online voter regigration APIs"
-__url__ = "https://github.com/vote/ovrlib"
+__description__ = "Library to interface with per-state online voter registration APIs"
+__url__ = "https://github.com/vote/state-registration-apis"
 __version__ = "0.0.2"
 __author__ = "Sage Weil"
 __author_email__ = "sage@newdream.net"
