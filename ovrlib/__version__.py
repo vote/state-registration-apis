7945db1bb3b4780f987727b1211a7a901baa2c67
Sage Weil <sage@redhat.com>
2020-06-30 09:13:06 -0500

    0.0.2

--- ovrlib/__version__.py
+++ ovrlib/__version__.py
@@ -3,7 +3,7 @@
 __title__ = "ovrlib"
 __description__ = "Library to interface with per-state online voter regigration APIs"
 __url__ = "https://github.com/vote/ovrlib"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Sage Weil"
 __author_email__ = "sage@newdream.net"
 __license__ = "Apache 2.0"
