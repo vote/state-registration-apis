ec939bf73fa9b1a7a42a040664902b2c7d8bbd25
Ben Weissmann <ben@voteamerica.com>
2020-12-23 17:39:56 -0500

    0.0.6

--- ovrlib/__version__.py
+++ ovrlib/__version__.py
@@ -3,7 +3,7 @@
 __title__ = "ovrlib"
 __description__ = "Library to interface with per-state online voter registration APIs"
 __url__ = "https://github.com/vote/state-registration-apis"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __author__ = "Sage Weil"
 __author_email__ = "sage@newdream.net"
 __license__ = "Apache 2.0"
